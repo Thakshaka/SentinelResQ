a45030c73c16ff1242acc912e4af4f8be793969b
Thakshaka Rathnayake <76155498+Thakshaka@users.noreply.github.com>
2023-06-25 16:15:04 +0530

    Update Sensor_New.ino

--- Sensor_New.ino
+++ Sensor_New.ino
@@ -1,12 +1,16 @@
 #include <dht.h>
+#include <MQ2.h>
 #include <ArduinoJson.h>
 
 dht DHT11;
 
+MQ2 mq2(A0);
+float mq2_lpg, mq2_co, mq2_smoke;
+
 void setup() {
   Serial.begin(115200);
   
-  pinMode(A0, INPUT); // MQ2
+  mq2.begin(); // MQ2
   pinMode(A1, INPUT); // MQ135
 }
 
@@ -16,7 +20,9 @@ void loop() {
   float humidity = DHT11.humidity;
   delay(250);
 
-  float mq2 = analogRead(A0);
+  mq2_lpg = mq2.readLPG();
+  mq2_co = mq2.readCO();
+  mq2_smoke = mq2.readSmoke();
   delay(250);
 
   float mq135 = analogRead(A1);
@@ -26,8 +32,10 @@ void loop() {
   StaticJsonDocument<1024> doc;
   doc["temperature"] = temperature;
   doc["humidity"] = humidity;
-  doc["mq2"] = mq2;
-  doc["mq135"] = mq135;
+  doc["lpg"] = mq2_lpg;
+  doc["co"] = mq2_co;
+  doc["smoke"] = mq2_smoke;
+  doc["air_quality"] = mq135;
 
   // Serialize the JSON object to a string
   String sensorData;
