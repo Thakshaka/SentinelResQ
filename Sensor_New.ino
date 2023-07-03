efe62a7e5df8d50cf16fb03da0e2de706a734ef3
Thakshaka Rathnayake <76155498+Thakshaka@users.noreply.github.com>
2023-07-03 18:20:39 +0530

    Update Sensor_New.ino

--- Sensor_New.ino
+++ Sensor_New.ino
@@ -1,47 +1,81 @@
 #include <dht.h>
 #include <MQ2.h>
+#include "MQ135.h"
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
 #include <ArduinoJson.h>
 
-dht DHT11;
+// DHT22
+#define DHT22PIN 7
+dht DHT;
 
+// MQ2
 MQ2 mq2(A0);
-float mq2_lpg, mq2_co, mq2_smoke;
+
+// MQ135
+MQ135 mq135(A1);
+
+// BMP180
+Adafruit_BMP085 bmp180;
 
 void setup() {
   Serial.begin(115200);
-  
-  mq2.begin(); // MQ2
-  pinMode(A1, INPUT); // MQ135
+  mq2.begin();
+  bmp180.begin();
+  warmupSensors();
 }
 
+float temperature, humidity, co2, lpg, pressure;
+
 void loop() {
-  DHT11.read11(7); // DHT11
-  float temperature = DHT11.temperature;
-  float humidity = DHT11.humidity;
-  delay(250);
+  temperature = readTemperature();
+  humidity = readHumidity();
+  co2 = readCO2();
+  lpg = readLPG();
+  pressure = readPressure();
 
-  mq2_lpg = mq2.readLPG();
-  mq2_co = mq2.readCO();
-  mq2_smoke = mq2.readSmoke();
-  delay(250);
+  // Create a StaticJsonDocument
+  StaticJsonDocument<40> jsonDoc;
 
-  float mq135 = analogRead(A1);
-  delay(250);
+  // Assign values to the JSON document
+  jsonDoc["temperature"] = temperature;
+  jsonDoc["humidity"] = humidity;
+  jsonDoc["co2"] = co2;
+  jsonDoc["lpg"] = lpg;
+  jsonDoc["pressure"] = pressure;
 
-  // Create a JSON object
-  StaticJsonDocument<1024> doc;
-  doc["temperature"] = temperature;
-  doc["humidity"] = humidity;
-  doc["lpg"] = mq2_lpg;
-  doc["co"] = mq2_co;
-  doc["smoke"] = mq2_smoke;
-  doc["air_quality"] = mq135;
+  // Serialize the JSON document to a string
+  String jsonString;
+  serializeJson(jsonDoc, jsonString);
 
-  // Serialize the JSON object to a string
-  String sensorData;
-  serializeJson(doc, sensorData);
+  // Print the JSON string
+  Serial.println(jsonString);
 
-  Serial.println(sensorData);
+  delay(1000);
+}
 
-  delay(250);
+void warmupSensors() {
+  delay(20000);
+}
+
+float readTemperature() {
+  DHT.read22(DHT22PIN);
+  return DHT.temperature;
+}
+
+float readHumidity() {
+  DHT.read22(DHT22PIN);
+  return DHT.humidity;
+}
+
+float readCO2() {
+  return mq135.getPPM();
+}
+
+float readLPG() {
+  return mq2.readLPG();
+}
+
+float readPressure() {
+  return bmp180.readPressure();
 }
